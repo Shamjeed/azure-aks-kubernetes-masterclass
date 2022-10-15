f028d8c7f73e657a55f235ab3fb35612cdd77fc0
Shamjeed <shamjeedtc@gmail.com>
2022-10-15 13:55:42 +0400

    Azure aks cluster creation commands

--- 01-Create-AKS-Cluster/azure-aks-cluster.ps1
+++ 01-Create-AKS-Cluster/azure-aks-cluster.ps1
@@ -1,3 +1,6 @@
+#Azure Help Reource
+https://learn.microsoft.com/en-us/azure/aks/learn/quick-kubernetes-deploy-cli 
+
 $ResourseGroupName="aks-rg1"
 $KubernetesClusterName= "aksdemo1"
 $Location="eastus"
@@ -30,3 +33,26 @@ kubectl get namespaces
 kubectl get pods --all-namespaces
 
 # For more commands Please check C:\Users\shamj\IdeaProjects\azure-aks-kubernetes-masterclass\01-Create-AKS-Cluster\README.md
+
+kubectl apply -f kube-manifests/
+
+# Verify Pods
+kubectl get pods
+
+# Verify Deployment
+kubectl get deployment
+
+# Verify Service (Make a note of external ip)
+kubectl get service
+
+# Access Application
+http://<External-IP-from-get-service-output>
+
+http://20.62.187.66
+
+# Delete Applications
+kubectl delete -f kube-manifests/
+
+# Delete cluster
+$ResourseGroupName="aks-rg1"
+az group delete --name $ResourseGroupName --yes --no-wait
